982bd6738e9b9afdb922228d423dbc56a0832a61
samsivabalan <samesh.sivabalan@gmail.com>
2025-03-22 19:39:59 +0000

    updated installation script

--- scripts/installation-scripts/install_suricata.sh
+++ scripts/installation-scripts/install_suricata.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-echo "Updating the system..."
-sudo apt-get update && sudo apt-get upgrade -y
-
 echo "Installing required packages..."
 sudo apt-get install -y python-pip libnss3-dev liblz4-dev libnspr4-dev libcap-ng-dev git
 
